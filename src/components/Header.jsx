c2da4c1d461aadbcfc96cfb90ed9939bde66e2a0
AbdAlmjed Skaik <ab0d-33@hotmail.com>
2023-02-05 19:50:24 +0200

    .

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import { useStateContext } from '../context/ContextProvider';
 import { Select, Tooltip, Button, IconButton, Menu, MenuButton, MenuItem, MenuList, Modal, ModalBody, ModalCloseButton, ModalContent, ModalFooter, ModalHeader, ModalOverlay, Table, TableContainer, Tbody, Td, Th, Thead, Tr, useColorMode, useDisclosure } from '@chakra-ui/react';
 import { TbFileInfo, TbLayoutSidebar, TbSettings } from 'react-icons/tb';
 import { FiRotateCcw, FiRotateCw, FiZoomIn, FiZoomOut } from 'react-icons/fi';
-import { MdDarkMode, MdFullscreen, MdOutlineLightMode } from 'react-icons/md';
+import { MdDarkMode, MdOutlineLightMode } from 'react-icons/md';
 import logo from "../assets/images/horizontal-logo.png";
 import { zoomOptions } from '../data/viewerData';
 import { Kbd } from '@chakra-ui/react';
@@ -30,9 +30,8 @@ const ToolbarBtn = ({ icon, canActive, title, handleClick, initStatus, className
     </Tooltip>
   )
 }
-const DocumentInformation = () => {
+const DocumentInformation = ({ data, numOfPages }) => {
   const { isOpen, onOpen, onClose } = useDisclosure();
-
   return (
     <div>
       <ToolbarBtn
@@ -40,7 +39,7 @@ const DocumentInformation = () => {
         title="Show Document Information"
         handleClick={onOpen}
       />
-      <Modal isOpen={isOpen} onClose={onClose}>
+      <Modal isOpen={isOpen} size="lg" onClose={onClose}>
         <ModalOverlay />
         <ModalContent>
           <ModalHeader>Document Information</ModalHeader>
@@ -57,8 +56,8 @@ const DocumentInformation = () => {
                 </Thead>
                 <Tbody>
                   <Tr>
-                    <Td>SALIC</Td>
-                    <Td>stsadmin@salic.onmicrosoft.com</Td>
+                    <Td>{data.Name}</Td>
+                    <Td>{data.Email}</Td>
                   </Tr>
                 </Tbody>
               </Table>
@@ -71,7 +70,7 @@ const DocumentInformation = () => {
                 </Thead>
                 <Tbody>
                   <Tr>
-                    <Td>27-08-2020 05:09</Td>
+                    <Td>{data.inviteAt}</Td>
                   </Tr>
                 </Tbody>
               </Table>
@@ -83,7 +82,7 @@ const DocumentInformation = () => {
                 </Thead>
                 <Tbody>
                   <Tr>
-                    <Td>78</Td>
+                    <Td>{numOfPages}</Td>
                   </Tr>
                 </Tbody>
               </Table>
@@ -102,11 +101,18 @@ const DocumentInformation = () => {
 };
 
 
-const Header = () => {
-  const { currentMode, setMode, setActiveThumbnailes, handleFullScreen, handleRotateLeft, handleRotateRight, handleZoomIn, handleZoomOut, setZoom } = useStateContext();
+
+const Header = ({ docSchema }) => {
+  const { currentMode, scale, setMode, setActiveThumbnailes, handleFullScreen, handleRotateLeft, handleRotateRight, handleZoomIn, handleZoomOut, setZoom } = useStateContext();
   const { toggleColorMode } = useColorMode();
   const [isShowTools, setIsShowTools] = useState(false);
 
+  const setScaleByParentWidth = () => {
+    const parentWidth = document.getElementById("pagesParentRef")?.clientWidth * 0.9;
+    const firstPageWidth = docSchema.pages[0].width;
+    const newScale = parentWidth / firstPageWidth;
+    return newScale
+  }
 
   const ViewerTools = () => (
     <div className='flex gap-2'>
@@ -127,8 +133,9 @@ const Header = () => {
         handleClick={handleZoomOut}
       />
       <div>
-        <Select variant='filled' defaultValue={1} onChange={e => setZoom(Number(e.target.value))}>
-          {zoomOptions.map((option, i) => <option key={i} value={option.value}>{option.label}</option>)}
+        <Select variant='filled' value={scale} onChange={e => setZoom(Number(e.target.value))}>
+          <option value={setScaleByParentWidth()} selected>Fit</option>
+          {zoomOptions.map((option, i) => <option key={i} value={option.value} selected={option.selected}>{option.label}</option>)}
         </Select>
       </div>
       <ToolbarBtn
@@ -170,7 +177,7 @@ const Header = () => {
           <img src={logo} alt="logo" className="hidden md:block" style={{ height: 30, filter: currentMode === "dark" ? "grayscale(1) invert(1)" : "" }} />
         </div>
         {!  isShowTools && <div className='flex gap-2'>
-          <DocumentInformation />
+          <DocumentInformation data={docSchema.invitor} numOfPages={docSchema.numOfPages} />
           <Menu>
             <MenuButton
               as={IconButton}
--- src/pages/ESignDocument.jsx
+++ src/pages/ESignDocument.jsx
@@ -4,19 +4,58 @@ import Header from "../components/Header";
 import { docSchema } from "../data/docSchema";
 import { Scrollbars } from 'react-custom-scrollbars-2';
 import wait from "waait";
-import { Spinner } from "@chakra-ui/react";
+import { Button, Slider, SliderFilledTrack, SliderThumb, SliderTrack, Spinner, Tooltip } from "@chakra-ui/react";
+// import { fabric } from 'fabric';
+// import Konva from 'konva';
+// import { Stage, Layer, Rect, Text } from 'react-konva';
 
+
+function SliderThumbs({ getSliderValue }) {
+  const [sliderValue, setSliderValue] = React.useState(4);
+  const [showTooltip, setShowTooltip] = React.useState(false);
+
+  useEffect(() => {
+    getSliderValue(sliderValue);
+  }, [sliderValue]);
+  return (
+    <Slider
+      id='slider'
+      defaultValue={sliderValue}
+      min={1}
+      max={10}
+      colorScheme='blue'
+      onChange={(v) => setSliderValue(v)}
+      onMouseEnter={() => setShowTooltip(true)}
+      onMouseLeave={() => setShowTooltip(false)}
+    >
+      <SliderTrack>
+        <SliderFilledTrack />
+      </SliderTrack>
+      <Tooltip
+        bg='blue.800'
+        color='white'
+        placement='bottom'
+        isOpen={showTooltip}
+        label={sliderValue}
+      >
+        <SliderThumb />
+      </Tooltip>
+    </Slider>
+  )
+}
 
 const Thumbs = ({ pages }) => {
   const { activePage, setActivePage } = useStateContext();
+  const [sliderValue, setSliderValue] = React.useState(5);
 
   return (
     <div className='w-80 bg-neutral-200 dark:bg-neutral-800 flex-col overflow-hidden resize-x p-4 hidden md:flex'>
-      <div className="thumbs-header">
+      <div className="thumbs-header mb-4">
+        <SliderThumbs getSliderValue={setSliderValue} />
         {/* <h3 className="font-semibold text-xl text-text-color dark:text-white">Thumbnails</h3> */}
       </div>
         <Scrollbars>
-          <div className='thumbs-body flex flex-col items-center gap-4 h-full '>
+          <div className='thumbs-body flex justify-center flex-wrap gap-y-4 gap-x-2'>
             {
               pages?.map((item) => (
                 <div 
@@ -27,12 +66,12 @@ const Thumbs = ({ pages }) => {
                     const element = document.getElementById(`page_${item.Index}`);
                     if (element) element.scrollIntoView();
                   }}
-                  className={`thumb-item flex flex-col items-center w-fit mx-auto cursor-pointer hover:bg-neutral-300 dark:hover:bg-neutral-600 ${activePage === item.Index ? "bg-neutral-300 dark:bg-neutral-700" : ""} px-10 py-6 rounded-lg`}
+                  className={`thumb-item flex flex-col items-center w-fit h-fit cursor-pointer hover:bg-neutral-300 dark:hover:bg-neutral-600 ${activePage === item.Index ? "bg-neutral-300 dark:bg-neutral-700" : ""} px-10 py-6 rounded-lg`}
                 >
                   <div></div>
                   <div 
                     className={`bg-white shadow-xl border-gray-300 border-2 ${activePage === item.Index && "border-active-color"}`} 
-                    style={{width: item.width / 4, height: item.height / 4}}
+                    style={{width: (item.width / 4) * (sliderValue / 4), height: (item.height / 4) * (sliderValue / 4)}}
                   >
 
                   </div>
@@ -48,7 +87,6 @@ const Thumbs = ({ pages }) => {
     </div>
   )
 }
-
 
 const Page = ({item, totalPages, setDocumentSchema}) => {
   const pageRef = useRef();
@@ -73,6 +111,15 @@ const Page = ({item, totalPages, setDocumentSchema}) => {
   }, []);
 
 
+  // const canvas = new fabric.Canvas(`canvas__page__${item.Index}`, {
+  //   width: item.width * scale,
+  //   height: item.height * scale,
+  //   backgroundColor: "#fff",
+  // });
+  // useEffect(() => {
+  //   canvas.setWidth(item.width * scale);
+  //   canvas.setHeight(item.height * scale);
+  // }, [scale])
   return (
     <div key={item.Index} className="page-item mb-4" id={`page_${item.Index}`} ref={pageRef}>
       <div 
@@ -80,6 +127,7 @@ const Page = ({item, totalPages, setDocumentSchema}) => {
         style={{width: item.width * scale, height: item.height * scale, transform: `rotate(${rotation}deg)`}}
       >
         {!item.isLoaded ? <Spinner size='xl' /> : item.Index}
+        {/* <canvas id={`canvas__page__${item.Index}`} /> */}
       </div>
       <div className='flex justify-between'>
         <div>file.png</div>
@@ -89,37 +137,47 @@ const Page = ({item, totalPages, setDocumentSchema}) => {
   )
 }
 const Document = ({ pages, actions, setDocumentSchema }) => {
-  const { activePage, setActivePage, scale } = useStateContext();
+  const pagesParentRef = useRef(null);
+  const { activePage, setActivePage, scale, setZoom } = useStateContext();
 
+  // const setScaleByParentWidth = () => {
+  //   const parentWidth = pagesParentRef.current.clientWidth * 0.9;
+  //   const firstPageWidth = pages[0].width;
+  //   const newScale = parentWidth / firstPageWidth;
+  //   setZoom(newScale);
+  //   console.log("newScale", firstPageWidth);
+  // }
+  // useEffect(() => {
+  //   setScaleByParentWidth();
+  // }, []);
   return (
     <div className="flex-[7] flex flex-col bg-neutral-100 px-4 dark:bg-neutral-700 overflow-auto">
       <div className="pages-header">
         <div className="flex justify-center p-2 overflow-auto">
           <div className='flex gap-2 items-center'>
             {actions?.map((item, i) => (
-              <button 
-                key={i}
+              <Button
+                key={i} 
+                colorScheme='gray'
+                variant='outline'
+                size="xs"
                 onClick={() => {
                   setActivePage(item.page);
                   const element = document.getElementById(`page_${item.page}`);
                   if (element) element.scrollIntoView();
                 }} 
-                className="bg-gray-300 hover:bg-blue-active-color text-gray-500 dark:text-gray-50 dark:bg-main-dark-bg dark:hover:bg-blue-active-color hover:text-white w-6 h-6 rounded-full hover:scale-105  active:scale-100 transition-all"
-              >
-                <span>{i+1}</span>
-              </button>
+              >{item.page}</Button>
             ))}
           </div>
         </div>
       </div>
       <div className='pages-body h-full'>
         <Scrollbars style={{ height: "100%" }}>
-          <div className='pages block h-full'>
+          <div ref={pagesParentRef} id="pagesParentRef" className='pages block h-full'>
               {
                 pages?.map((item) => (
-                  <div className="mx-auto" style={{ width: item.width * scale }}>
+                  <div key={item.Index} className="mx-auto" style={{ width: item.width * scale }}>
                     <Page
-                      key={item.Index}
                       item={item} 
                       totalPages={pages.length} 
                       setDocumentSchema={setDocumentSchema}
@@ -136,15 +194,13 @@ const Document = ({ pages, actions, setDocumentSchema }) => {
 }
 
 
-
-
 const ESignDocument = () => {
   const [documentSchema, setDocumentSchema] = useState(docSchema);
   const { activeThumbnailes } = useStateContext();
 
   return (
     <div className="w-screen h-screen overflow-hidden bg-white">
-      <Header />
+      <Header docSchema={documentSchema} />
       {/* <Toolbar /> */}
 
       <main className="flex h-full pt-14 md:pt-16">
